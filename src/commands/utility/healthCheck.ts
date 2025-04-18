b51aec373b995d11243b9485caa4075c88c63285
fazlulShanto <fazlulcse17@gmail.com>
2025-04-18 18:53:28 +0600

    fix health check command

--- src/commands/utility/healthCheck.ts
+++ src/commands/utility/healthCheck.ts
@@ -6,6 +6,8 @@ import {
 } from 'discord.js';
 import type { SlashCommand } from '../../config/command-handler';
 import { getStoreConfigFromCache } from '@/utils/redis';
+import { PremiumInfoDAL } from '@/db/premium-info.dal';
+import { format, formatDuration, intervalToDuration } from 'date-fns';
 
 export const HealthCheckCommand: SlashCommand = {
   name: 'health-check',
@@ -16,19 +18,42 @@ export const HealthCheckCommand: SlashCommand = {
   requiredPermissions: ['Administrator', 'PremiumOrTrial'],
   execute: async (interaction: ChatInputCommandInteraction) => {
     const storeConfig = await getStoreConfigFromCache(interaction.guildId ?? '');
-
+    const premiumInfo = await PremiumInfoDAL.getPremiumInfoByGuildId(interaction.guildId ?? '');
+    const isPremium = premiumInfo?.isPremium;
+    const isTrial = premiumInfo?.isTrial;
+    const statusText = isPremium ? 'Premium' : isTrial ? 'Trial' : 'Free';
+    const expiryDate = premiumInfo?.isPremium
+      ? premiumInfo.premiumExpiryDate
+        ? new Date(premiumInfo.premiumExpiryDate)
+        : 'None'
+      : premiumInfo?.trialEndDate
+        ? new Date(premiumInfo.trialEndDate)
+        : 'None';
     const embed = new EmbedBuilder()
-      .setTitle('Health Check')
-      .setDescription(`Store config: ${JSON.stringify(storeConfig)}`)
+      .setTitle('Server Health Check')
+      .setDescription(`**Premium status: ${statusText}**`)
       .addFields(
         {
-          name: 'Bot Admin Role ID',
-          value: storeConfig.botAdminRoleId,
+          name: 'Bot Admin Role',
+          value: `<@&${storeConfig.botAdminRoleId}>`,
           inline: true,
         },
         {
           name: 'Currency',
           value: storeConfig.currency,
+          inline: true,
+        },
+        {
+          name: `${statusText} Expiry Date`,
+          value: expiryDate ? format(expiryDate, 'MM/dd/yyyy HH:mm:ss') : 'None',
+          inline: true,
+        },
+        {
+          name: `${statusText} Remaining`,
+          value:
+            isPremium || isTrial
+              ? `\`${formatDuration(intervalToDuration({ start: new Date(), end: expiryDate }), { format: ['months', 'days', 'hours', 'minutes'] })}\``
+              : 'None',
           inline: true,
         },
       )
